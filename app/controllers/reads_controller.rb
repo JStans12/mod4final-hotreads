5d0f4d22b9ceb0c4f99ae38b9dfbaabd1f739fe4
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-02-27 13:01:44 -0700

    hot and hottest reads calculated on hotreads

---
+++ app/controllers/reads_controller.rb
@@ -0,0 +1,7 @@
+class ReadsController < ApplicationController
+
+  def index
+    @hot_reads = Read.hot
+  end
+
+end
--- app/models/read.rb
+++ app/models/read.rb
@@ -1,3 +1,17 @@
 class Read < ApplicationRecord
   validates_presence_of :url
+
+  scope :hot, -> {
+    select('reads.url as url')
+      .where('reads.created_at > ?', Time.now - 1.day)
+      .group("reads.url")
+      .order('count("reads".id) DESC').limit(10)
+  }
+
+  scope :hottest, -> {
+    select('reads.url as url')
+      .where('reads.created_at > ?', Time.now - 1.day)
+      .group("reads.url")
+      .order('count("reads".id) DESC').limit(1).first
+  }
 end
