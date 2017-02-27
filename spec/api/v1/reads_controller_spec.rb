43c7210fffefd51f5388547b7d0eadc6d5e0a2d8
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-02-27 13:36:34 -0700

    hot reads endpoint tested

--- spec/api/v1/reads_controller_spec.rb
+++ spec/api/v1/reads_controller_spec.rb
@@ -9,4 +9,20 @@ describe Api::V1::ReadsController do
       expect(Read.first.url).to eq("some_url")
     end
   end
+
+  context "index" do
+    it "renders the hottest reads around" do
+      create_list(:read, 11)
+      url = Read.first.url
+      create(:read, url: url)
+
+      get api_v1_reads_path
+      reads = JSON.parse(response.body, symbolize_names: true)
+
+      url_exists = reads.any? { |read| read[:url] == url }
+
+      expect(url_exists).to eq(true)
+      expect(reads.length).to eq(10)
+    end
+  end
 end
